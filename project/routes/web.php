585e47d15de64317ad74b216610f6395c3786866
Raveniv <raveniv@ya.ru>
2025-02-25 23:34:18 +0500

    second work

--- project/routes/web.php
+++ project/routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/userform', [\App\Http\Controllers\FormProcessorController::class, 'index']);
+Route::post('/store_form', [\App\Http\Controllers\FormProcessorController::class, 'store']);
