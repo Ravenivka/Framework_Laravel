c0a4479dd5c2acc5144754579ffa4398759f8e46
Ravenivka <raveniv@ya.ru>
2025-03-15 10:41:12 +0500

    lesson6

--- requestProject/routes/web.php
+++ requestProject/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,5 +11,8 @@ Route::get('/index' , [EmployeeController::class, 'index']) ;
 Route::post('/store-form', [EmployeeController::class,'store']) ;
 //Route::get('/view' , [EmployeeController::class, 'view']) ;  debug route
 Route::get('/user/{id}', [EmployeeController::class,'update'])  ;
+
+Route::get('/show' , [BookController::class, 'show']) ;
+Route::post('/book-store', [BookController::class,'store']) ;
 
 
